ca0b464f893b0897b9fcee7cbaaeb92ceaf76e49
Murilo Jorge <murilojorge62@gmail.com>
2026-08-23 00:08:20 -0300

    Testando as funções da classe Pokemon

--- src/main/java/game/jogopokemon/JogoPokemon.java
+++ src/main/java/game/jogopokemon/JogoPokemon.java
@@ -1,8 +1,59 @@
 package game.jogopokemon;
+import game.modelos.Pokemon;
 
 public class JogoPokemon {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+        
+        System.out.println("===== INICIANDO O JOGO =====");
+        
+        // Nome, Nivel, HP Maximo, Velocidade,
+        // Ataque, Ataque Especial, Defesa, Defesa Especial
+        Pokemon meuPokemon = new Pokemon("Charizard", 40, 78, 100, 84, 109, 78, 85);
+        
+        System.out.println("Um " + meuPokemon.getName() + " apareceu!");
+        System.out.println("Nivel: " + meuPokemon.getLevel());
+        System.out.println("HP: " + meuPokemon.getHPNow() + "/" + meuPokemon.getHPMax());
+        System.out.println("Fainted?: " + meuPokemon.isDesmaiado());
+        
+        System.out.println("\n===== SIMULANDO UM ATAQUE =====");
+        
+        // Testando o receber dano
+        System.out.println(meuPokemon.getName() + " tomou 15 de dano!");
+        meuPokemon.receberDano(15);
+        System.out.println("HP: " + meuPokemon.getHPNow() + "/" + meuPokemon.getHPMax());
+        System.out.println("Fainted?: " + meuPokemon.isDesmaiado() + "\n");
+
+        // Testando receber reviver (desmaiado false)
+        System.out.println("Utilizando reviver (30% HP Max)");
+        meuPokemon.curarRevive(30);
+        System.out.println("HP: " + meuPokemon.getHPNow() + "/" + meuPokemon.getHPMax());
+        System.out.println("Fainted?: " + meuPokemon.isDesmaiado() + "\n");
+        
+        // Testando receber pocao (desmaiado false)
+        System.out.println("Utilizando pocao (+20 HP)");
+        meuPokemon.curarPocao(20);
+        System.out.println("HP: " + meuPokemon.getHPNow() + "/" + meuPokemon.getHPMax());
+        System.out.println("Fainted?: " + meuPokemon.isDesmaiado() + "\n");
+
+        // Testando o receber dano (crítico)
+        System.out.println(meuPokemon.getName() + " tomou 100 de dano!");
+        meuPokemon.receberDano(100);
+        System.out.println("HP: " + meuPokemon.getHPNow() + "/" + meuPokemon.getHPMax());
+        System.out.println("Fainted?: " + meuPokemon.isDesmaiado() + "\n");
+        
+        // Testando receber pocao (desmaiado true)
+        System.out.println("Utilizando super pocao (+60 HP)");
+        meuPokemon.curarPocao(60);
+        System.out.println("HP: " + meuPokemon.getHPNow() + "/" + meuPokemon.getHPMax());
+        System.out.println("Fainted?: " + meuPokemon.isDesmaiado() + "\n");
+        
+        // Testando receber reviver (desmaiado false)
+        System.out.println("Utilizando reviver (30% HP Max)");
+        meuPokemon.curarRevive(30);
+        System.out.println("HP: " + meuPokemon.getHPNow() + "/" + meuPokemon.getHPMax());
+        System.out.println("Fainted?: " + meuPokemon.isDesmaiado() + "\n");
+        
+    } // FIM DA MAIN
+    
+} // FIM DA CLASSE
