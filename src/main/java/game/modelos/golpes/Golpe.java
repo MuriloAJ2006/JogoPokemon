d880323b033378f24d04a0e4c45d8b1f594cb3c3
Murilo Jorge <murilojorge62@gmail.com>
2026-08-23 14:37:15 -0300

    Pacote e classes para o sistema de golpes

---
+++ src/main/java/game/modelos/golpes/Golpe.java
@@ -0,0 +1,59 @@
+package game.modelos.golpes;
+import game.modelos.Pokemon;
+import game.modelos.TipoElemento;
+
+public abstract class Golpe {
+    
+    // ATRIBUTOS
+    private String nome;
+    private String descricao;
+    private TipoElemento tipo;
+    private int ppMax;
+    private int ppNow;
+    
+    // =======================================================
+
+    // CONSTRUTOR
+    public Golpe(String nome, String descricao,
+                  TipoElemento tipo, int ppMax) {
+        
+        this.nome = nome;
+        this.descricao = descricao;
+        this.tipo = tipo;
+        this.ppMax = ppMax;
+        this.ppNow = ppMax;
+        
+    } // FIM DO CONSTRUTOR
+
+    // =======================================================
+
+    // GETTERS E SETTERS
+    // Nome do Golpe
+    public String getNome() {return this.nome;}
+    public void setNome(String nome) {this.nome = nome;}
+    
+    // Descricao do Golpe
+    public String getDescricao() {return this.descricao;}
+    public void setDescricao(String descricao) {this.descricao = descricao;}
+    
+    // Tipo do Golpe
+    public TipoElemento getTipo() {return this.tipo;}
+    public void setTipo (TipoElemento tipo) {this.tipo = tipo;}
+    
+    // PP Maximo do Golpe
+    public int getPPMax() {return this.ppMax;}
+    public void setPPMax(int ppMax) {this.ppMax = ppMax;}
+    
+    // PP Atual do Golpe
+    public int getPPNow() {return this.ppNow;}
+    public void setPPNow(int ppNow) {this.ppNow = ppNow;}
+    
+    // =======================================================
+
+    // METODO ABSTRATO
+    // Os itens podem ser usados de maneiras diferentes.
+    public abstract void executar(Pokemon atacante, Pokemon defensor);
+    
+    // =======================================================
+    
+} // FIM DA CLASSE
