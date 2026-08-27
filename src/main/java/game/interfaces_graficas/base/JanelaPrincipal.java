175e3b49b699d15a1a25630f8fca5a4a02691f38
Murilo Jorge <murilojorge62@gmail.com>
2026-08-26 23:50:11 -0300

    Organização dos pacotes extras

---
+++ src/main/java/game/interfaces_graficas/base/JanelaPrincipal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game.interfaces_graficas.base;
+
+/**
+ *
+ * @author muril
+ */
+public class JanelaPrincipal {
+    
+}
