0272f15a1cbc39d63dfff71dd3aec91baa18e455
Murilo Jorge <murilojorge62@gmail.com>
2026-08-28 11:41:27 -0300

    Adição do Cálculo de Dano Final dos Golpes

--- src/main/java/game/mecanicas/batalha/DamageCalculator.java
+++ src/main/java/game/mecanicas/batalha/DamageCalculator.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package game.mecanicas.batalha;
 
-/**
- *
- * @author muril
- */
+import game.modelos.pokemon.Pokemon;
+import game.modelos.golpes.Move;
+import game.modelos.enums.MoveCategory;
+
 public class DamageCalculator {
+    
+    private DamageCalculator() {}
+    
+    public static int calcDamage(Pokemon atacante, Pokemon defensor, Move golpe) {
+        
+        if (golpe.getCategory() == MoveCategory.STATUS) return 0;
+        
+        int level = atacante.getStats().getLevel();
+        int power = golpe.getPower();
+        int atk, def;
+        
+        if (golpe.getCategory() == MoveCategory.PHYSICAL) {
+            atk = atacante.getStats().getActualStats().getAttack();
+            def = defensor.getStats().getActualStats().getDefense();
+        } else{
+            atk = atacante.getStats().getActualStats().getSpAttack();
+            def = defensor.getStats().getActualStats().getSpDefense();
+        }
+        
+        int danoBase = (((2 * level) / 5 + 2) * power * atk / def) / 50 + 2;
+        double typeMultiplier = TypeMachupUtils.getMultiplier(golpe.getType(), defensor.getSpecies());
+        double rngFactor = (Math.random() * 16 + 85) / 100.0;
+        double stab = 1.0;
+        
+        if (golpe.getType() == atacante.getSpecies().getPrimaryType() ||
+            (atacante.getSpecies().hasSecondaryType() && golpe.getType() == atacante.getSpecies().getSecondaryType())) {
+            stab = 1.5;
+        } // FIM DO IF
+        
+        int danoFinal = (int)(danoBase * stab * typeMultiplier * rngFactor);
+        
+        if (danoFinal == 0 && typeMultiplier > 0) {
+            danoFinal = 1;
+        } else if (typeMultiplier == 0){
+            danoFinal = 0;
+        }
+            
+        return danoFinal;
+    }
     
 }
